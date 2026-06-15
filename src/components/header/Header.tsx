18676aa6dab3fe5d38f69eff6526379133519a98
zlidzien <m.karsakou@gmail.com>
2026-06-15 09:32:36 +0200

    adjust naming

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,10 @@
 const Header = () => (
   <header className="header">
     <div className="container header-inner">
-      <h1 className="logo">MissileSila</h1>
+      <h1 className="logo">Launch Shaft</h1>
       <nav className="nav">
         <a href="#">Home</a>
-        <a href="#">Missiles WiKi</a>
-        <a href="#">Ships WiKi</a>
+        <a href="#">Let's Rocket It</a>
         <a href="#">About</a>
       </nav>
       <button>Dark Mode</button>
