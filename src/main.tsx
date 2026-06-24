c6d82abc19a40322f78870c8a7518844901828a4
zlidzien <m.karsakou@gmail.com>
2026-06-24 14:30:23 +0200

    update routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/launch_shaft">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
